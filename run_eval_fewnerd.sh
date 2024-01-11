23b9cc57c66ee32661cc29388fe82770282dd496
yaor <55626719+yaomiaoqiu@users.noreply.github.com>
2024-01-11 18:27:30 +0800

    Add files via upload
    只进行评估

---
+++ run_eval_fewnerd.sh
@@ -0,0 +1,21 @@
+
+datasets_target=('FEW-NERD-INTRA')
+n_way_k_shots=('5_1')
+
+for _dataset_target in "${datasets_target[@]}"
+   do
+
+              for _n_way_k_shot in "${n_way_k_shots[@]}"
+                   do
+                        python3 main.py --mode use_type_name \
+                                        --train False \
+                                        --dataset_source $_dataset_target \
+                                        --dataset_target $_dataset_target \
+                                        --n_way_k_shot $_n_way_k_shot \
+                                        --seed 42 \
+                                        --test_stage1_only False\
+                                        --test_stage2_only False \
+                                        --filter True
+                   done
+   done
+
